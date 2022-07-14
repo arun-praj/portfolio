6c72d3d079bcaaab3ec70a91dc54c58101173db6
Arun Prajapati <arunkp1122@gmail.com>
2022-07-14 19:13:13 +0545

    Added Disclaimer

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
       <div>
          <Head>
             <title>@Aaaarun</title>
-            <meta name='description' content='Portfolio of Arun Prajapati' />
+            <meta name='description' content='Author: Arun Prajapati, Portfolio of Arun Prajapati, Length:2 pages' />
             <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
             <meta httpEquiv='Content-Type' content='text/html; charset=ISO-8859-1'></meta>
             <link rel='icon' href='/favicon.ico' />
