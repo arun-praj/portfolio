77abc67963246c8683882c182bdfa2273bd1ce6c
Arun Prajapati <arunkp1122@gmail.com>
2022-07-26 12:47:25 +0545

    Added subdomain movie and resource

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
+   rewrites: [
+      {
+         source: '/:path*',
+         has: [
+            {
+               type: 'host',
+               value: 'resource.arunprajapati.com',
+            },
+         ],
+         destination: '/resource',
+      },
+      {
+         source: '/:path*',
+         has: [
+            {
+               type: 'host',
+               value: 'movie.arunprajapati.com',
+            },
+         ],
+         destination: '/movie',
+      },
+   ],
 }
 
 module.exports = nextConfig
