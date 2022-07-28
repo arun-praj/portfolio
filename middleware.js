983d3e6c97c37423888d6d6351285ebb188f66d6
Arun Prajapati <arunkp1122@gmail.com>
2022-07-28 17:27:48 +0545

    Fixed static files not loading in productiion

--- middleware.js
+++ middleware.js
@@ -2,12 +2,23 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
    let wildcard = request.headers.get('host').split('.')[0]
    // console.log(request.nextUrl.clone())
+   console.log(wildcard)
+   const { pathname } = request.nextUrl
+   if (
+      pathname.startsWith('/api') || //  exclude all API routes
+      pathname.startsWith('/static') || // exclude static files
+      pathname.includes('.') // exclude all files in the public folder
+   ) {
+      return NextResponse.next()
+   }
    if (wildcard === 'movie') {
       let url = request.nextUrl.clone()
-      return NextResponse.rewrite(url + '_sites/movie' + url.pathname)
+      // url.pathname =
+      return NextResponse.rewrite(url + '_sites/movie')
    } else if (wildcard === 'resource') {
       let url = request.nextUrl.clone()
       return NextResponse.rewrite(url + '_sites/resource' + url.pathname)
    }
-   return NextResponse.rewrite(request.nextUrl.clone())
+   // return NextResponse.rewrite(request.nextUrl.clone())
+   return NextResponse.next()
 }
