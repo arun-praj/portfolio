e95e3ef1534fc16c2fe304345a575657fee0528f
Arun Prajapati <arunkp1122@gmail.com>
2022-12-12 07:10:45 -0800

    Fixed typescript error

--- pages/api/note.ts
+++ pages/api/note.ts
@@ -18,6 +18,7 @@ export default async function handler(
       case 'GET':
          try {
             const foundNote = await prisma.note.findFirst({
+               //@ts-ignore
                where: { id: query.note_id },
             })
             res.status(200).json({ note: foundNote, success: true })
