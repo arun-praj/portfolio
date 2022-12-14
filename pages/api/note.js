c11bf7b7ba0411ce6c40a8b08e593504ee5b7fd8
Arun Prajapati <arunkp1122@gmail.com>
2022-12-14 09:11:11 -0800

    Check ts build fix

---
+++ pages/api/note.js
@@ -0,0 +1,46 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { create } from 'domain'
+
+import prisma from '../../lib/prisma'
+
+// import { PrismaClient } from '@prisma/client'
+
+export default async function handler(req, res) {
+   const { query, method, body } = req
+
+   switch (method) {
+      case 'GET':
+         try {
+            // break
+            const foundNote = await prisma.note.findFirst({
+               //@ts-ignore
+               where: { id: query.note_id },
+            })
+
+            res.status(200).json({ note: foundNote, success: true })
+         } catch (e) {
+            res.status(404).json({ success: false })
+         }
+         break
+      case 'POST':
+         try {
+            const createdNote = await prisma.note.upsert({
+               where: { id: body.note_id },
+               update: {
+                  content: JSON.stringify(body.content),
+               },
+               create: {
+                  content: JSON.stringify(body.content),
+               },
+            })
+            prisma.$disconnect()
+            res.status(200).json({ success: true, note: createdNote })
+         } catch (e) {
+            res.status(500).json({
+               success: false,
+               error: `Something went horrible: ${e}`,
+            })
+         }
+         break
+   }
+}
